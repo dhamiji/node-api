09934bfd42438d6fb228f89572e43806792f3824
veerabhadra <veerabhadra.sajjan@gmail.com>
2021-03-23 15:13:19 +0530

    crypto token

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express()
 const db = require('./queries')
-const port = 3000
+const port = 3000;
 
 app.use(bodyParser.json())
 app.use(
@@ -18,8 +18,12 @@ app.get('/', (request, response) => {
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
 app.post('/register', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
+app.put('/update/:id', db.updateUser)
+app.delete('/delete/:id', db.deleteUser)
+app.post('/login', db.login)
+app.post('/forgetPassword', db.forgetPassword)
+app.get('/userByToken', db.getUserByToken)
+app.post('/resetPassword/:username', db.resetPassword)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
--- api/queries.js
+++ api/queries.js
@@ -1,4 +1,14 @@
-const Pool = require('pg').Pool
+// import sha256 from 'crypto-js/sha256';
+// import hmacSHA512 from 'crypto-js/hmac-sha512';
+// import Base64 from 'crypto-js/enc-base64';
+
+// const message, nonce, path, privateKey; // ...
+// const hashDigest = sha256(nonce + message);
+// const hmacDigest = Base64.stringify(hmacSHA512(path + hashDigest, privateKey));
+// var AES = require("crypto-js/aes");
+// var SHA256 = require("crypto-js/sha256");
+var CryptoJS = require("crypto-js");
+const { Pool } = require('pg')
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
@@ -8,9 +18,21 @@ const pool = new Pool({
 
 })
 
-const sgMail = require('@sendgrid/mail')
+var tempPass = ""
+
+const sgMail = require('@sendgrid/mail');
+const { json } = require("express");
 sgMail.setApiKey("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")
 
+const randomString = () => {
+  var result = '';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < 7; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
@@ -32,49 +54,55 @@ const getUserById = (request, response) => {
   })
 }
 
-const createUser = (request, response) => {
+const createUser = async (request, response) => {
   const { first_name, last_name, email, password, username } = request.body
+  var user = await checkUser(username)
+  if (user.username) {
+    response.status(409).json({ error: true, message: "Username already exist!" })
+  } else {
+    pool.query(`INSERT INTO users (first_name, last_name, email, password, username) 
+              VALUES ($1, $2, $3, $4, $5)`, [first_name, last_name, email, password, username],
+      (error, results) => {
+        if (error) {
+          response.status(500).send(error)
+        }
+        else {
+          const msg = {
+            to: email, // Change to your recipient
+            from: 'chetan.dhami@xcdify.com', // Change to your verified sender
+            subject: 'Registration Done',
+            text: 'This mail is to let you know that you are successfully registered with Caltrack',
+            html: '<strong>This mail is to let you know that you are successfully registered with Caltrack</strong>',
+          }
+          sgMail
+            .send(msg)
+            .then(() => {
+              console.log('Email sent')
+              response.status(200).json({ error: false, message: "User Created Successfully!" })
+            })
+            .catch((error) => {
+              response.status(500).send(error)
+            })
 
-  pool.query('INSERT INTO users (first_name, last_name, email, password, username) VALUES ($1, $2, $3, $4, $5)', [first_name, last_name, email, password, username], (error, results) => {
-    if (error) {
-      response.send(`User Already Exists`)
-      throw error
-    }
-    else {
-      const msg = {
-        to: email, // Change to your recipient
-        from: 'chetan.dhami@xcdify.com', // Change to your verified sender
-        subject: 'Registration Done',
-        text: 'This mail is to let you know that you are successfully registered with Caltrack',
-        html: '<strong>This mail is to let you know that you are successfully registered with Caltrack</strong>',
-      }
-      sgMail
-        .send(msg)
-        .then(() => {
-          console.log('Email sent')
+        }
 
-        })
-        .catch((error) => {
-          console.error(error)
-        })
-      response.status(201).send(`User added with ID: ${results}`)
-    }
+      })
+  }
 
-  })
 }
 
 const updateUser = (request, response) => {
-  const id = parseInt(request.params.id)
-  const { name, email } = request.body
+  const username = parseInt(request.params.username)
+  const { first_name, last_name, email } = request.body
 
   pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
+    'UPDATE users SET first_name=$1, last_name=$2, email=$3, WHERE username = $4',
+    [first_name, last_name, email, username],
     (error, results) => {
       if (error) {
         throw error
       }
-      response.status(200).send(`User modified with ID: ${id}`)
+      response.status(200).send(`User modified with ID: ${username}`)
     }
   )
 }
@@ -90,10 +118,97 @@ const deleteUser = (request, response) => {
   })
 }
 
+const checkUser = (username) => {
+  return new Promise((resolve, reject) => {
+    pool.query('SELECT * FROM users WHERE username = $1', [username], (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      resolve(results.rows[0]);
+    })
+  });
+}
+
+const login = async (request, response) => {
+  const { username, password } = request.body;
+  const user = await checkUser(username);
+  if (user && user.password && user.password == password) {
+    var token = CryptoJS.AES.encrypt(JSON.stringify(user), 'secret key 123').toString();
+    response.status(200).json({ error: false, token: token })
+  } else if (user && password == tempPass) {
+    response.status(200).json({ error: false, message: "Success", username: username })
+  } else if (user && user.password && user.password != password) {
+    response.json({ error: true, message: "You've entered a wrong password!" })
+  }
+  else {
+    response.json({ error: true, message: "User does not exist!" })
+  }
+}
+
+const getUserByToken = async (request, response) => {
+  const { token } = request.headers;
+  var bytes = CryptoJS.AES.decrypt(token, 'secret key 123');
+  var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+  response.status(200).json({ error: false, user: decryptedData })
+}
+
+
+const forgetPassword = async (request, response) => {
+  const { email } = request.body;
+  const temp = randomString()
+  tempPass = temp;
+  const msg = {
+    to: email, // Change to your recipient
+    from: 'chetan.dhami@xcdify.com', // Change to your verified sender
+    subject: 'Password Reset',
+    text: `To reset your password, Please login using the give temporary password.`,
+    html: `<div>
+      <strong>To reset your password, Please login using the give temporary password.</strong>
+      <div>${tempPass}</div>
+    </div>`,
+  }
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent')
+
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  response.json({ code: tempPass, message: "Mail sent successfully!" })
+}
+
+const resetPassword = async (request, response) => {
+  const username = request.params.username
+  const { password } = request.body
+  const user = await checkUser(username);
+  if (user.username) {
+    pool.query(
+      'UPDATE users SET password=$1 WHERE username = $2',
+      [password, username],
+      (error, results) => {
+        if (error) {
+          response.status(500).send(error)
+        } else {
+          response.status(200).send({ error: false, message: "Password Updated Successfully" })
+        }
+
+      }
+    )
+  }
+
+}
+
+
 module.exports = {
   getUsers,
   getUserById,
   createUser,
   updateUser,
   deleteUser,
+  login,
+  forgetPassword,
+  getUserByToken,
+  resetPassword,
 }
